65b737dcc80d809004571a37225d6fc75c977835
Alexandre DELORME <alexandre.delorme@viacesi.fr>
2025-03-26 11:40:47 +0100

    fix header persistence

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import Headroom from "react-headroom";
 import "./Header.scss";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
@@ -8,15 +8,19 @@ import {
   workExperiences,
   skillsContent,
   resumeSection,
-  educationInfo
+  educationInfo,
 } from "../../portfolio";
 
 function Header() {
-  const {isDark} = useContext(StyleContext);
+  const { isDark } = useContext(StyleContext);
   const viewExperience = workExperiences.display;
   const viewEducation = educationInfo.display;
   const viewSkills = skillsContent.display;
   const viewResume = resumeSection.display;
+
+  const closeMenu = () => {
+    document.getElementById("menu-btn").checked = false;
+  };
 
   return (
     <Headroom>
@@ -27,37 +31,33 @@ function Header() {
           <span className="grey-color">/&gt;</span>
         </a>
         <input className="menu-btn" type="checkbox" id="menu-btn" />
-        <label
-          className="menu-icon"
-          htmlFor="menu-btn"
-          style={{color: "white"}}
-        >
+        <label className="menu-icon" htmlFor="menu-btn" style={{ color: "white" }}>
           <span className={isDark ? "navicon navicon-dark" : "navicon"}></span>
         </label>
         <ul className={isDark ? "dark-menu menu" : "menu"}>
           {viewSkills && (
             <li>
-              <a href="#skills">Skills</a>
+              <a href="#skills" onClick={closeMenu}>Skills</a>
             </li>
           )}
           {viewEducation && (
             <li>
-              <a href="#education">Education</a>
+              <a href="#education" onClick={closeMenu}>Education</a>
             </li>
           )}
           {viewExperience && (
             <li>
-              <a href="#experience">Work Experiences</a>
+              <a href="#experience" onClick={closeMenu}>Work Experiences</a>
             </li>
           )}
-          {(
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-          )}
+          <li>
+            <a href="#projects" onClick={closeMenu}>Projects</a>
+          </li>
           {viewResume && (
             <li>
-              <a href="https://4lexdel.github.io/Portfolio/assets/src/CV.pdf?lang=en">Resume</a>
+              <a href="https://4lexdel.github.io/Portfolio/assets/src/CV.pdf?lang=en" onClick={closeMenu}>
+                Resume
+              </a>
             </li>
           )}
           <li>
@@ -71,4 +71,5 @@ function Header() {
     </Headroom>
   );
 }
+
 export default Header;
